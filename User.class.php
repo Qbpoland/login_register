5917cc86c503be60d9d5426c3b1c16c926931a89
Qbpoland <jakub.stasiuk2005@gmail.com>
2022-11-23 10:05:28 +0100

    user

--- User.class.php
+++ User.class.php
@@ -1,34 +1,60 @@
 <?php
-class user {
+class User {
     private int $id;
-    private string $login; 
-    private string $passwordHash;
+    private mysqli $db;
+    private string $login;
+    private string $password;
     private string $firstName;
-    private string $lastName 
+    private string $lastName;
 
-    public function __construct(string $login, string $password)
-    {
-       $this->login = $login 
-       $this->password_hash = password_hash($password, PASSWORD_ARGON2I);
-    }
-    
-    public function isAuth() : bool {
-
-    }
-    public function login(){
-
-    }
-    public function logout(){
-
-    }
-    public function register(){
-
+    public function __construct(string $login, string $password) {
+        $this->login = $login;
+        $this->password = $password;
+        $this->firstName = "";
+        $this->lastName = "";
+        global $db;
+        $this->db = &$db;
     }
 
+    public function register() : bool {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->firstName, $this->lastName);
+        $result = $preparedQuery->execute();
+        return $result;
+    }
 
+    public function login() : bool {
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
+                $this->id = $row['id'];
+                $this->firstName = $row['firstName'];
+                $this->lastName = $row['lastName'];
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    public function setFirstName(string $firstName) {
+        $this->firstName = $firstName;
+    }
+    public function setLastName(string $lastName) {
+        $this->lastName = $lastName;
+    }
+    public function getName() : string {
+        return $this->firstName . " " . $this->lastName;
+    }
 }
-
-
-
-
 ?>
--- register.php
+++ register.php
@@ -11,15 +11,30 @@
 
     <div id=register>
 <h1>Zarejestruj się</h1>
-<form action="index.php" method="post">
-    <label for="emailInput">Email:</label>
-    <input type="email" name="email" id="emailInput">
-    <label for="passwordInput">Hasło:</label>
-    <input type="password" name="password" id="passwordInput">
-    <label for="passwordRepeatInput">Hasło ponownie:</label>
-    <input type="password" name="passwordRepeat" id="passwordRepeatInput">
-    <input type="hidden" name="action" value="register">
-    <input type="submit" value="Zarejestruj">
-    </div>
+<form action="" method="post">
+        <label for="loginID">Login:</label><br>
+        <input type="text" name="login" id="loginID"><br>
+        <label for="passwordID">Hasło:</label><br>
+        <input type="password" name="password" id="passwordID"><br>
+        <label for="firstNameID">Imię:</label><br>
+        <input type="text" name="firstName" id="firstNameID"><br>
+        <label for="lastNameID">Nazwisko:</label><br>
+        <input type="text" name="lastName" id="lastNameID"><br>
+        <input type="submit" value="Rejestruj">
+    </form>
+<?php
+if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+    require_once('config.php');
+    require_once('class/User.class.php');
+    $user = new User($_REQUEST['login'], $_REQUEST['password']);
+    $user->setFirstName($_REQUEST['firstName']);
+    $user->setLastName($_REQUEST['lastName']);
+    if($user->register()) {
+        echo "Zarejestrowano poprawnie";
+    } else {
+        echo "Błąd rejestracji użytkownika";
+    }
+}
+?>    
 </body>
 </html>
