02f2f161835700729cbcefa74c2f552e481e5241
Qbpoland <jakub.stasiuk2005@gmail.com>
2022-11-23 10:14:22 +0100

    all

--- index.php
+++ index.php
@@ -1,31 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_login.css">
-    <title>Logowanie</title>
-</head>
-<body>
-    
-    <div id="login">
-        <h1>Zaloguj się</h1>
-        <form action="index.php" method="post">
-            <label for="emailInput">Email:</label>
-            <input type="email" name="email" id="emailInput">
-            <br>
-            <label for="passwordInput">Hasło:</label>
-            <input type="password" name="password" id="passwordInput">
-            <input type="hidden" name="action" value="login">
-            <input type="submit" value="Zaloguj">
-            <br>
-            <br>
-            
-        </form>
-        <a href="register.php">Zarejestruj się </a>
+<?php
+require_once('config.php');
+require_once('class/User.class.php');
 
+$user = new User('jkowalski', 'tajneHasło');
+/*
+if($user->register()) {
+    echo "Zarejestrowano poprawnie";
+} else {
+    echo "Błąd rejestracji użytkownika";
+}
+*/
 
-    </div>
-</body>
-</html>
+if($user->login()) {
+    echo "Zalogowano poprawnie";
+} else {
+    echo "Błędny login lub hasło";
+}
+
+?>
