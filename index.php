acc9e52958edd7bca2fa5b98c036434a3eefb8e4
Qbpoland <jakub.stasiuk2005@gmail.com>
2022-11-30 11:35:45 +0100

    commit

--- index.php
+++ index.php
@@ -6,11 +6,40 @@ use Steampixel\Route;
  require_once('User.class.php');
 
 
- Route::add('/login' , function() {
+ Route::add('/' , function() {
     echo "strona główna";
  });
 
- Route::run('/login');
+ Route::add('/login', function(){
+    global $twig;
+    $twig->display('login.html.twig');
+ });
+
+ Route::add('/login', function(){
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+    
+    
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            
+            $v = array(
+                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+            );
+            $twig->display('message.html.twig', $v);
+        } else {
+            
+            $twig->display('message.html.twig', 
+                                ['message' => "Błędny login lub hasło"]);
+        }
+    } else {
+        
+        die("błąd - nie otrzymano danych!");
+    }
+ }, 'post');
+
+Route::run('/login');
+
 
 
  ?>
